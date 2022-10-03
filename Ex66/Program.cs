9211ea64a149e37ef9fc794ceb90cd5e708eeb7c
led Ice <Andrey-ssk98@yandex.ru>
2022-10-04 01:45:09 +0300

    ex66

---
+++ Ex66/Program.cs
@@ -0,0 +1,17 @@
+﻿//Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+//M = 1; N = 15 -> 120
+//M = 4; N = 8. -> 30
+
+Console.WriteLine("Задайте значение M");
+int start = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Задайте значение N");
+int end = Convert.ToInt32(Console.ReadLine());
+
+int sum = 0;
+int num = start;
+while(num <= end)
+{
+    sum = sum + num; num++;
+}
+Console.WriteLine(sum);
